4dd351fd50cb87135283ad0b8617e63f8c834c5e
Mitran Matei-Cristian <matei.mitran10@gmail.com>
2023-08-29 19:16:10 +0200

    Setup

---
+++ euroseven-backend/src/main/java/com/titi/euro7/Euro7Application.java
@@ -0,0 +1,13 @@
+package com.titi.euro7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Euro7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Euro7Application.class, args);
+    }
+
+}
---
+++ euroseven-backend/src/test/java/com/titi/euro7/Euro7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.titi.euro7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Euro7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
