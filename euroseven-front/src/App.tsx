3b29bb6f9870d463391fd6239b01b4583d49fba3
Mitran Matei-Cristian <matei.mitran10@gmail.com>
2023-09-07 18:43:02 +0200

    Created card

--- euroseven-front/src/App.tsx
+++ euroseven-front/src/App.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, IconButton, Typography } from "@mui/material";
 import logo1 from "./assets/logo1.png";
 import logo2 from "./assets/logo2.png";
 import f1 from "./assets/f1.jpeg";
@@ -6,8 +6,13 @@ import f2 from "./assets/f2.jpeg";
 import f3 from "./assets/f3.jpeg";
 import f4 from "./assets/f4.jpeg";
 import f5 from "./assets/f5.jpeg";
+import card1 from "./assets/card1.jpeg";
 import { useState, useEffect } from "react";
 import "./App.css";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import SearchIcon from "@mui/icons-material/Search";
+import MenuIcon from "@mui/icons-material/Menu";
+import SendIcon from "@mui/icons-material/Send";
 
 function App() {
   const images = [f1, f2, f3, f4, f5];
@@ -21,6 +26,10 @@ function App() {
 
     return () => clearInterval(imageSwitchInterval);
   }, [images]);
+
+  const changeImage = (index: number) => {
+    setCurrentImageIndex(index);
+  };
 
   return (
     <Box>
@@ -92,7 +101,7 @@ function App() {
         className="jumbotron-container"
         sx={{
           width: "100%",
-          height: "100vh",
+          height: "80vh",
           overflow: "hidden", // Prevent horizontal scrollbar
           position: "relative",
         }}
@@ -102,7 +111,7 @@ function App() {
             key={index}
             height="100%"
             src={image}
-            alt="jumbotron image"
+            alt="jumbotron"
             className={`jumbotron ${
               index === currentImageIndex ? "active" : ""
             }`}
@@ -114,7 +123,11 @@ function App() {
               top: 0, // Position images at the top
               left: 0, // Position images at the left
               opacity: index === currentImageIndex ? 1 : 0, // Apply the fade-in effect to the active image
-              transition: "opacity 1s ease", // Smooth transition for opacity
+              animation:
+                index === currentImageIndex
+                  ? "zoom-in 20s ease-in-out"
+                  : "none", // Apply the zoom-in animation to the active image
+              transition: "opacity 1s ease",
             }}
           />
         ))}
@@ -129,13 +142,186 @@ function App() {
             transform: "translate(-50%, -50%)",
             padding: "20px",
             color: "#fff" /* Text color */,
+            display: "flex",
           }}
         >
-          <img
-            height="100px"
-            src={logo2}
-            alt="logo2"
-          />
+          <img height="100px" src={logo2} alt="logo2" />
+          <Box sx={{ ml: "25vh", mt: "3vh" }}>
+            <Button
+              variant="text"
+              sx={{ mr: "25px", fontWeight: "bold", color: "#0054a6" }}
+            >
+              Clienti Rezidentiali
+            </Button>
+            <Button
+              variant="text"
+              sx={{ mr: "25px", fontWeight: "bold", color: "#0054a6" }}
+            >
+              Clienti Business
+            </Button>
+            <Button
+              variant="text"
+              sx={{ mr: "25px", fontWeight: "bold", color: "#0054a6" }}
+            >
+              Electripedia
+            </Button>
+            <Button
+              variant="text"
+              sx={{ mr: "25px", fontWeight: "bold", color: "#0054a6" }}
+            >
+              Servicii Online
+            </Button>
+            <Button
+              variant="text"
+              sx={{ mr: "25px", fontWeight: "bold", color: "#0054a6" }}
+            >
+              Informatii Utile
+            </Button>
+            <Button
+              variant="text"
+              sx={{ fontWeight: "bold", color: "#0054a6" }}
+            >
+              Plafonare
+            </Button>
+          </Box>
+          <Box sx={{ ml: "34vh", mt: "2vh" }}>
+            <IconButton>
+              <PermIdentityIcon
+                sx={{
+                  mr: "10px",
+                  width: "30px",
+                  height: "35px",
+                  color: "#0054a6",
+                }}
+              />
+            </IconButton>
+            <IconButton>
+              <SearchIcon
+                sx={{
+                  mr: "10px",
+                  width: "30px",
+                  height: "35px",
+                  color: "#0054a6",
+                }}
+              />
+            </IconButton>
+            <IconButton>
+              <MenuIcon
+                sx={{ width: "30px", height: "35px", color: "#0054a6" }}
+              />
+            </IconButton>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            position: "absolute",
+            bottom: "0",
+            left: "50%",
+            transform: "translateX(-50%)",
+            display: "flex",
+            mb: "15px",
+          }}
+        >
+          {images.map((_, index) => (
+            <div
+              key={index}
+              className={`circle ${
+                index === currentImageIndex ? "active" : ""
+              }`}
+              onClick={() => changeImage(index)}
+            ></div>
+          ))}
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: "#F4F4F4",
+          width: "140vh",
+          height: "45vh",
+          margin: "0 auto",
+          mt: "10vh",
+          position: "relative",
+          display: "flex",
+        }}
+      >
+        <Typography
+          variant="body1"
+          component="div"
+          sx={{
+            fontWeight: "bold",
+            position: "absolute",
+            top: 0,
+            left: "50%",
+            transform: "translateX(-50%)",
+            padding: "10px",
+          }}
+        >
+          DESCOPERA OFERTELE DEDICATE
+        </Typography>
+
+        <Box
+          sx={{
+            width: "50vh",
+            height: "25vh",
+            position: "relative",
+            top: "25%",
+            display: "flex",
+            ml: "5vh",
+            mb: "10vh",
+          }}
+        >
+          <Box
+            sx={{
+              height: "25vh",
+              width: "35vh",
+            }}
+          >
+            <img
+              src={card1}
+              alt="card1"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+              }}
+            />
+          </Box>
+          <Box
+            sx={{
+              width: "35vh",
+              height: "20vh",
+              float: "right",
+              backgroundColor: "white",
+              position: "absolute",
+              top: "10%",
+              right: "10%",
+              textAlign: "center"
+            }}
+          >
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{
+                fontWeight: "bold",
+                color: "#0054a6",
+                padding: "10px"
+              }}
+            >
+              Plăteşte Factura Online
+            </Typography>
+            <Typography
+              variant="body1"
+              component="div"
+              sx={{
+                padding: "10px"
+              }}
+            >
+              Este simplu, rapid şi sigur să plăteşti online factura Euro7
+            </Typography>
+            <Button variant="contained" endIcon={<SendIcon />} sx={{margin: "0 auto", backgroundColor: "#0054a6", width: "50%"}}>
+              Plăteşte
+            </Button>
+          </Box>
         </Box>
       </Box>
     </Box>
